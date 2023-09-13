7b63615f66dd447cdfaf447fe2a50f95325b8e6b
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2023-09-13 14:48:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 Socket = SocketIO(app, cors_allowed_origins="*")
-
+CORS(app)
 @app.route('/')
 def index():
     return render_template('index.html')
